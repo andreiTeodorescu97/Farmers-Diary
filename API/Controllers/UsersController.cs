9783e58e0a843566a8d177cdb2e16dc642252598
andreiTeodorescu997 <andrei.teodorescu997@gmail.com>
2022-04-11 18:12:36 +0300

    Added first controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Domain.DataContext;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class UsersController : JFBaseController
+    {
+        private readonly JFContext _dbContext;
+
+        public UsersController(JFContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public  async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _dbContext.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _dbContext.Users.FindAsync(id);
+        }
+    }
+}
