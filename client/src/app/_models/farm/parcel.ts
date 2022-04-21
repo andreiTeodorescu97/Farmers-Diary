4979a40dcbde8f39a7f887ddb00465980d9139fd
andreiTeodorescu997 <andrei.teodorescu997@gmail.com>
2022-04-21 11:20:49 +0300

    add parcel model

---
+++ client/src/app/_models/farm/parcel.ts
@@ -0,0 +1,10 @@
+export interface EditParcel {
+    id: number;
+    name: string;
+    surface: number;
+    cultureName: string;
+    cultureId: number;
+    countyId: number;
+    surfaceName: string;
+  }
+  
